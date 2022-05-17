4eb2e7ff5afc8b68ec4eb2b1f604e7e22e0083a5
cesarcamachonava <105687889+cesarcamachonava@users.noreply.github.com>
2022-05-16 21:37:05 -0500

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,47 @@
+/*
+*@param {Numero}n1,n2
+*@return {Numero}res
+*/
+
+function suma(n1,n2){
+    n1 = parseInt(n1);
+    n2 = parseInt(n2);
+    let res = n1 + n2;
+    return res;
+}
+
+/*
+*@param {Numero}nu1,nu2
+*@return {Numero}res1
+*/
+
+function resta(nu1,nu2){
+    nu1 = parseInt(nu1);
+    nu2 = parseInt(nu2);
+    let res1 = nu1 - nu2;
+    return res1;
+}
+
+/*
+*@param {Numero}num1,num2
+*@return {Numero}res2
+*/
+
+function multiplicacion(num1,num2){
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    let res2 = num1 * num2;
+    return res2;
+}
+
+/*
+*@param {Numero}nume1,nume2
+*@return {Numero}res3
+*/
+
+function divicion(nume1,nume2){
+    nume1 = parseInt(nume1);
+    nume2 = parseInt(nume2);
+    let res3 = nume1 / nume2;
+    return res3;
+}
